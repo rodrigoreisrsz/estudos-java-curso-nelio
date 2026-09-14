9396e3a711f8d20941863ad6a8608567da548d52
Rodrigo Reis <rodrigoreis15822@gmail.com>
2026-09-14 11:04:10 -0300

    revisando list

---
+++ src/main/java/com/reis/revisao/list/Main.java
@@ -0,0 +1,13 @@
+package com.reis.revisao.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Rodrigo");
+        //nomes.add(8); -> compilador nao permite pois ele sabe que a List espera valores em String
+    }
+}
